30cc9e742d863b5b8df8e318ef82ce18b1545953
igorp984 <igorp984@gmail.com>
2018-01-04 17:24:37 -0300

    tentando resolver problema gunicorn

--- defesa/wsgi.py
+++ defesa/wsgi.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "defesa.settings")
 
-
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "heroku_blog.settings.production")
 application = DjangoWhiteNoise(application)
