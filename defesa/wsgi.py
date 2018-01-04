940bf26fd6167dad18c65244f2b4fa3ba49e0c8d
igorp984 <igorp984@gmail.com>
2018-01-04 16:02:22 -0300

    configurando o acesso ao banco no heroku

--- defesa/wsgi.py
+++ defesa/wsgi.py
@@ -14,6 +14,6 @@ from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 from whitenoise.django import DjangoWhiteNoise
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "heroku_blog.settings.production")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "heroku_blog.settings.production")
 application = DjangoWhiteNoise(application)
 
