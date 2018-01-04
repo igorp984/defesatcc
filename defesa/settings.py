357a527a4d1be7db7036935895825e67ff8e39ba
igorp984 <igorp984@gmail.com>
2018-01-04 15:47:15 -0300

    configurando o acesso ao banco no heroku

--- defesa/settings.py
+++ defesa/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -85,6 +85,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+DATABASES['default'].update(db_from_env)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
