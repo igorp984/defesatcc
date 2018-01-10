29dcbf8f2d7edb8d78556d14b2c0f79859ff4244
igorp984 <igorp984@gmail.com>
2018-01-10 16:54:05 -0300

    feita alterações

--- defesa/accounts/models.py
+++ defesa/accounts/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin, User
 
 class Usuario(AbstractBaseUser, PermissionsMixin):
 
-	username = models.Charfield('Nome do Usuário', max_length=30, unique=True)
+	username = models.CharField('Nome do Usuário', max_length=30, unique=True)
 	email = models.EmailField('E-mail', unique=True)
-	name = models.Charfield('Nome', max_length=200)
+	name = models.CharField('Nome', max_length=200)
 	is_active = models.BooleanField('Está ativo?', blank=True, default=True)
 	is_staff = models.BooleanField('É admin', blank=True, default=False)
 	date_joined = models.DateTimeField('Data de Entrada', auto_now_add=True)
