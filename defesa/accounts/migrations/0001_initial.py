49f9406c69f2b25902ce8a245860359896957982
igorp984 <igorp984@gmail.com>
2018-01-10 17:39:40 -0300

    teste de migrations

--- defesa/accounts/migrations/0001_initial.py
+++ defesa/accounts/migrations/0001_initial.py
@@ -3,9 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 from django.conf import settings
-import re
 import django.contrib.auth.models
 import django.core.validators
+import re
 
 
 class Migration(migrations.Migration):
@@ -18,18 +18,18 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Usuario',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
-                ('password', models.CharField(verbose_name='password', max_length=128)),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
                 ('last_login', models.DateTimeField(null=True, blank=True, verbose_name='last login')),
-                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
-                ('username', models.CharField(validators=[django.core.validators.RegexValidator(re.compile('^[\\w.@+-]+$', 32), 'o nome de usuário só pode conter letras, digitos os os seguintes caracteres @ . + - _', 'invalid')], unique=True, verbose_name='Nome do Usuário', max_length=30)),
-                ('email', models.EmailField(unique=True, verbose_name='E-mail', max_length=254)),
-                ('name', models.CharField(verbose_name='Nome', max_length=200)),
-                ('is_active', models.BooleanField(default=True, verbose_name='Está ativo?')),
-                ('is_staff', models.BooleanField(default=False, verbose_name='É admin')),
+                ('is_superuser', models.BooleanField(help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status', default=False)),
+                ('username', models.CharField(validators=[django.core.validators.RegexValidator(re.compile('^[\\w.@+-]+$', 32), 'o nome de usuário só pode conter letras, digitos os os seguintes caracteres @ . + - _', 'invalid')], max_length=30, verbose_name='Nome do Usuário', unique=True)),
+                ('email', models.EmailField(max_length=254, verbose_name='E-mail', unique=True)),
+                ('name', models.CharField(max_length=200, verbose_name='Nome')),
+                ('is_active', models.BooleanField(verbose_name='Está ativo?', default=True)),
+                ('is_staff', models.BooleanField(verbose_name='É admin', default=False)),
                 ('date_joined', models.DateTimeField(auto_now_add=True, verbose_name='Data de Entrada')),
-                ('groups', models.ManyToManyField(related_name='user_set', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_query_name='user', verbose_name='groups', to='auth.Group')),
-                ('user_permissions', models.ManyToManyField(related_name='user_set', blank=True, help_text='Specific permissions for this user.', related_query_name='user', verbose_name='user permissions', to='auth.Permission')),
+                ('groups', models.ManyToManyField(related_name='user_set', to='auth.Group', help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', blank=True, related_query_name='user', verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(related_name='user_set', to='auth.Permission', help_text='Specific permissions for this user.', blank=True, related_query_name='user', verbose_name='user permissions')),
             ],
             options={
                 'verbose_name_plural': 'Usuários',
@@ -42,11 +42,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='NovaSenha',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
-                ('key', models.CharField(unique=True, verbose_name='Chave', max_length=100)),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('key', models.CharField(max_length=100, verbose_name='Chave', unique=True)),
                 ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Criado em')),
-                ('confirmed', models.BooleanField(default=False, verbose_name='Confirmado?')),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='resets', verbose_name='Usuário')),
+                ('confirmed', models.BooleanField(verbose_name='Confirmado?', default=False)),
+                ('user', models.ForeignKey(related_name='resets', to=settings.AUTH_USER_MODEL, verbose_name='Usuário')),
             ],
             options={
                 'ordering': ['-created_at'],
