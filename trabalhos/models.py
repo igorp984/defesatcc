4b74130ee412d162fef19a682b5cc76d4efbc452
Igor Paim <igorp984@gmail.com>
2018-09-30 16:15:17 -0300

    criado o model DefesaTrabalho para agendamento da defesa com os relacionamento um pra um com trabalho e muitos pra muitos para banca de avaliação

--- trabalhos/models.py
+++ trabalhos/models.py
@@ -1,17 +1,48 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 from accounts.models import Usuario
+from django.db.models.signals import m2m_changed
+from django.core.exceptions import ValidationError
 
 # Create your models here.
-
+@python_2_unicode_compatible
 class Trabalhos(models.Model):
 
-	titulo = models.CharField('Título', max_length=150)
+	titulo = models.CharField('Título', max_length=250)
 	keywords = models.CharField('Palavras Chaves', max_length=150)
-	autor = models.CharField('Autor', max_length=150)
+	autor = models.CharField('Autor', max_length=250)
 	orientador = models.ForeignKey(Usuario, verbose_name='orientador', related_name='orientador')
-	co_orientador = models.CharField('Co Orientador', max_length=150, blank=True)
+	co_orientador = models.CharField('Co Orientador', max_length=250, blank=True)
 	resumo = models.TextField('Resumo', blank=True)
 	created_at = models.DateTimeField('Criado em', auto_now_add=True)
 	updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+	def __str__(self):
+		return self.titulo
+
+
+
+class DefesaTrabalho(models.Model):
+	BANCA_AVALIADORA_PENDENTE = 'pendente_banca_avaliadora'
+	AGENDADO = 'agendado'
+
+	STATUS_CHOICES = (
+		(BANCA_AVALIADORA_PENDENTE, 'Aguardando reposta ao convite para banca'),
+		(AGENDADO, 'Agendado'),
+	)
+
+	local = models.CharField('Local', max_length=250)
+	data = models.DateField('Data')
+	hora = models.TimeField('Horário')
+	trabalho = models.ForeignKey(Trabalhos, verbose_name='trabalho', related_name='trabalhos')
+	banca = models.ManyToManyField(Usuario)
+	status = models.CharField(
+		'Status', max_length=30,
+		choices=STATUS_CHOICES,
+		default=BANCA_AVALIADORA_PENDENTE)
+
+
+
 
--- trabalhos/views.py
+++ trabalhos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.views.generic import CreateView
 from django.views.generic.edit import UpdateView
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.contrib import messages
@@ -8,8 +10,8 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-from .models import Trabalhos
-from .forms import TrabalhoForm
+from .models import Trabalhos, DefesaTrabalho
+from .forms import TrabalhoForm, DefesaTrabalhoForm
 
 def cadastrar_trabalho(request):
 	template_name = 'trabalhos/forms.html'
@@ -69,3 +71,18 @@ class TrabalhoDetail(APIView):
 		trabalho = self.get_object(pk)
 		trabalho.delete()
 		return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class DefesaTrabablhoCreate(CreateView):
+	template_name = 'trabalhos/agendamento_cadastro.html'
+	model = DefesaTrabalho
+	form_class = DefesaTrabalhoForm
+	success_url = reverse_lazy(
+		"core:home"
+	)
+
+	def form_valid(self, form):
+		avaliador = form.cleaned_data['banca'][0]
+		print (avaliador.id)
+		messages.success(self.request, ("Agendamento realizado com sucesso"))
+		return super(DefesaTrabablhoCreate, self).form_valid(form)
